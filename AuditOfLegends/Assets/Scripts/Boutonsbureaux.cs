13b8638a5058a65d2d7357c2184bf1337fdfaeda
acouton <alexandre.couton@etu.unistra.fr>
2025-04-09 22:33:11 +0200

    finalisation click bureau écran employés

--- AuditOfLegends/Assets/Scripts/Boutonsbureaux.cs
+++ AuditOfLegends/Assets/Scripts/Boutonsbureaux.cs
@@ -3,62 +3,53 @@ using UnityEngine;
 public class Boutonsbureaux : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private GameObject boutonBureauEmployebasGauche;
-    private GameObject boutonBureauEmployebasDroite;
-    private GameObject boutonManager;
-    private GameObject boutonChefProjet;
-    private GameObject canvaManager;
-    private GameObject canvaEmployeBasGauche;
-    private GameObject canvaEmployeBasDroite;
-    private GameObject canvaChefProjet;
-    private GameObject map;
+    public GameObject boutonBureauEmployebasGauche;
+    public GameObject boutonBureauEmployebasDroite;
+    public GameObject boutonManager;
+    public GameObject boutonChefProjet;
+    public GameObject canvaManager;
+    public GameObject canvaEmployeBasGauche;
+    public GameObject canvaEmployeBasDroite;
+    public GameObject canvaChefProjet;
+    public GameObject map;
 
 
     void Start()
     {
-        boutonBureauEmployebasGauche = GameObject.Find("ButtonEmpBasGaucheH");
-        boutonBureauEmployebasDroite = GameObject.Find("ButtonEmpBasDroiteF");
-        boutonManager = GameObject.Find("ButtonManager");
-        boutonChefProjet = GameObject.Find("ButtonChefProjet");
 
-        canvaManager = GameObject.Find("BureauManager");
-        canvaEmployeBasGauche = GameObject.Find("BureauEmpM");
-        canvaEmployeBasDroite = GameObject.Find("BureauEmpF");
-        canvaChefProjet = GameObject.Find("BureauChefProjet");
-        map = GameObject.Find("Map");
     }
 
     public void clickBoutonBureauEmployeBasGauche()
     {
         Debug.Log("Button Employee Bottom Left clicked");
-        canvaEmployeBasGauche.enabled = true;
+        canvaEmployeBasGauche.SetActive(true);
         map.SetActive(false);
     }
     public void clickBoutonBureauEmployeBasDroite()
     {
         Debug.Log("Button Employee Bottom Right clicked");
-        canvaEmployeBasDroite.enabled = true;
+        canvaEmployeBasDroite.SetActive(true);
         map.SetActive(false);
     }
     public void clickBoutonBureauManager()
     {
         Debug.Log("Button Manager clicked");
-        canvaManager.enabled = true;
+        canvaManager.SetActive(true);
         map.SetActive(false);
     }
     public void clickBoutonBureauChefProjet()
     {
         Debug.Log("Button Project Manager clicked");
-        canvaChefProjet.enabled = true;
+        canvaChefProjet.SetActive(true);
         map.SetActive(false);
     }
     public void clickBoutonBureauFermer()
     {
         Debug.Log("Button Close clicked");
-        canvaEmployeBasGauche.enabled = false;
-        canvaEmployeBasDroite.enabled = false;
-        canvaManager.enabled = false;
-        canvaChefProjet.enabled = false;
+        canvaEmployeBasGauche.SetActive(false);
+        canvaEmployeBasDroite.SetActive(false);
+        canvaManager.SetActive(false);
+        canvaChefProjet.SetActive(false);
         map.SetActive(true);
     }
 
